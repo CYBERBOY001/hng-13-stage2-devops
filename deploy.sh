727e0de94f6096b231c2add5dd151e77dbcd37fc
HP@MKAEY <muhammadsulemankazaure@gmail.com>
2025-10-26 12:29:27 +0100

    new commit

--- deploy.sh
+++ deploy.sh
@@ -136,8 +136,8 @@ log INFO "Repository cloned/updated successfully."
 
 
 log INFO "Verifying project structure..."
-if [[ ! -f "docker-compose.yml" || ! -f ".env" ]]; then
-    log ERROR "Required files (docker-compose.yml) not found in $PWD"
+if [[ ! -f "docker-compose.yaml" || ! -f ".env" ]]; then
+    log ERROR "Required files (docker-compose.yaml) not found in $PWD"
     exit 1
 fi
 log INFO "Project files verified."
