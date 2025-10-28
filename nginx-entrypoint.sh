6cc71bdd5d7feae6eefa75db4fbb12977c051b63
HP@MKAEY <muhammadsulemankazaure@gmail.com>
2025-10-28 11:57:23 +0100

    new commit

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-set -e
-
-export PRIMARY="app_blue:8080"
-export BACKUP="app_green:8080"
 
 # Substitute only our custom vars, not Nginx internal ones
-envsubst '$PRIMARY $BACKUP' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
